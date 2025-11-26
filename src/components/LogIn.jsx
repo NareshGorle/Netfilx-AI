8213f73a37aa60dc9d14621f721c7f06b30c5d95
NareshGorle <nareshgorle776@gmail.com>
2025-11-26 10:04:11 +0530

    sign in & sign up validation

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import Header from "./Header"
+import { checkValidData } from "../utils/vadidate"
+
 
 const LogIn = () => {
-    const [isSignInForm, setIsSignInForm] = useState("")
+    const [isSignInForm, setIsSignInForm] = useState(true)
+    const [errMessage, setErrMessage] = useState("")
+
+    const name = useRef(null)
+    const email = useRef(null);
+    const password = useRef(null);
+
+    const handleFormValidate = () => {
+
+        const message = checkValidData(email.current.value, password.current.value, name.current.value)
+        setErrMessage(message)
+
+    }
+
     const toggleSignTitle = () => {
         setIsSignInForm(!isSignInForm)
     }
@@ -14,15 +29,36 @@ const LogIn = () => {
                     src="https://assets.nflxext.com/ffe/siteui/vlv3/6fd9d446-cd78-453a-8c9c-417ed3e00422/web/IN-en-20251117-TRIFECTA-perspective_2fe4e381-977f-49fd-a7f4-1da0bcf09429_small.jpg"
                     alt="bg-img" />
             </div>
-            <form className="absolute w-3/12 p-12  my-36 mx-auto right-0 left-0 text-white bg-black/80">
+            <form onSubmit={(e) => e.preventDefault()}
+                className="absolute w-3/12 p-12  my-36 mx-auto right-0 left-0 text-white bg-black/80">
                 <h1 className="text-3xl font-bold py-4">
                     {isSignInForm ? "Sign In" : "Sign Up"}
                 </h1>
-                <input type="text" placeholder="Email address" className="p-4 my-4 w-full bg-gray-600" />
-                <input type="Password" placeholder="Password" className="p-4 my-4 w-full bg-gray-600" />
-                <button className="bg-red-600 text-white p-4 my-4 w-full">
+                {!isSignInForm && <input
+                    ref={name}
+                    type="text"
+                    placeholder="Enter Name"
+                    className="p-4 my-4 w-full bg-gray-600" />}
+                <input
+                    type="text"
+                    placeholder="Email address"
+                    ref={email}
+                    className="p-4 my-4 w-full bg-gray-600" />
+
+                <input
+                    type="Password"
+                    ref={password}
+                    placeholder="Password"
+                    className="p-4 my-4 w-full bg-gray-600" />
+                <p className="text-red-600 font-bold text-lg py-2">{errMessage}</p>
+
+                <button
+                    className="bg-red-600 text-white p-4 my-4 w-full"
+                    onClick={handleFormValidate}>
                     {isSignInForm ? "Sign In" : "Sign Up"}
+
                 </button>
+
                 <h5 className="p-5 cursor-pointer " onClick={toggleSignTitle}>
                     {isSignInForm ? " New to Netfilx? Singn up now" : "Already registered? Sign in Now."}
                     .</h5>
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDFL10TtpaTGHR-vwtziVDW2ze6SGqXKg8",
+    authDomain: "netflix-ai-57eaf.firebaseapp.com",
+    projectId: "netflix-ai-57eaf",
+    storageBucket: "netflix-ai-57eaf.firebasestorage.app",
+    messagingSenderId: "65528061318",
+    appId: "1:65528061318:web:19ff9a0f14e8debac72007",
+    measurementId: "G-WN3VPKFQ16"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
